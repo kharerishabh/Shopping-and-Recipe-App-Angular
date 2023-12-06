c6c651899f74d34648ae5940af2e8dd9298c8a40
KHARERISHABH <rishabkhare41@gmail.com>
2023-12-07 00:16:32 +0530

    adding Navigation with Event binding and ngIf

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,4 @@
-import { AfterContentChecked, AfterContentInit, AfterViewChecked, AfterViewInit, Component, DoCheck, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
+import { Component } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
@@ -6,48 +6,11 @@ import { Ingredient } from '../shared/ingredient.model';
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
+export class ShoppingListComponent {
   // custom property binding example
   // @Input() element: {type: string, name: string, content: string}
   ingredients: Ingredient[] = [
     new Ingredient('Apples', 5),
     new Ingredient('Tomatos', 10),
   ]
-
-  constructor () {
-    console.log('Constructor Called')
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-      console.log('OnChange Called')
-      console.log(changes)
-  }
-
-  ngOnInit () {
-    console.log('ngOnInit called')
-  }
-
-  ngDoCheck () {
-    console.log('Docheck called')
-  }
-
-  ngAfterContentInit () {
-    console.log('after content init called')
-  }
-
-  ngAfterContentChecked() {
-    console.log('after content changed called')
-  }
-  ngAfterViewInit () {
-    console.log('after view init called')
-  }
-
-  ngAfterViewChecked() {
-    console.log('after view changed called')
-  }
-
-  ngOnDestroy(): void {
-      console.log('ngOnDestroy called')
-  }
-
 }

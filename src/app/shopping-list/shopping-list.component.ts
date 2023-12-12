b1cb0ec47bd4764c04c594507f412c034a006ba7
KHARERISHABH <rishabkhare41@gmail.com>
2023-12-12 15:31:33 +0530

    allow user to add ingredient to shopping list using event and property binding and view child event emitter

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -13,4 +13,8 @@ export class ShoppingListComponent {
     new Ingredient('Apples', 5),
     new Ingredient('Tomatos', 10),
   ]
+
+  onIngredientAdded(ingredient: Ingredient) {
+    this.ingredients.push(ingredient)
+  }
 }

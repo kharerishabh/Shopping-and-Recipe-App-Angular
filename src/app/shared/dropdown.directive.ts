e101e96f80ea176e3203a0329fa9072a7c1e36eb
KHARERISHABH <rishabkhare41@gmail.com>
2023-12-15 00:04:40 +0530

    building and using dropdown directive open the dropdown from the button and close it from anywhere

--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -1,11 +1,19 @@
-import { Directive, HostBinding, HostListener } from "@angular/core";
+import { Directive, ElementRef, HostBinding, HostListener } from "@angular/core";
 
 @Directive({
     selector: '[appDropDown]'
 })
 export class DropDownDirective {
-@HostBinding('class.open') isOpen = false
-@HostListener('click') toggleOpen (){
-    this.isOpen = !this.isOpen;
+//To Open and Close the dropdown from the button
+// @HostBinding('class.open') isOpen = false
+// @HostListener('click') toggleOpen (){
+//     this.isOpen = !this.isOpen;
+// }
+
+//To open from the button and close the dropdown from anywhere click
+@HostBinding('class.open') isOpen = false;
+@HostListener('document:click', ['$event']) toggleOpen(event: Event){
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
 }
+constructor(private elRef: ElementRef){}
 }

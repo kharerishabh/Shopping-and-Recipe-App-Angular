63b42cf9a89d2254ec50c5043e29131dc0f01a66
KHARERISHABH <rishabkhare41@gmail.com>
2023-12-18 13:22:55 +0530

    using Services for cross component communication

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
 
@@ -8,7 +8,6 @@ import { RecipeService } from '../recipe.service';
   styleUrl: './recipe-list.component.css'
 })
 export class RecipeListComponent implements OnInit{
-  @Output() recipeWasSelected = new EventEmitter<Recipe>()
   recipes: Recipe[];
   constructor(private recipeService: RecipeService){}
 
@@ -16,7 +15,4 @@ export class RecipeListComponent implements OnInit{
     this.recipes = this.recipeService.getRecipe()
   }
 
-  onRecipeSelected (recipe: Recipe) {
-    this.recipeWasSelected.emit(recipe)
-  }
 }

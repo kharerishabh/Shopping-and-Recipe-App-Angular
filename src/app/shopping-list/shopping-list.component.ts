23d109347cff95599a8092608de6ef9e3d21a467
KHARERISHABH <rishabkhare41@gmail.com>
2023-12-18 14:20:40 +0530

    adding shopplinglistService and using service for publishing data from A to B

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,20 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from './shopping-list.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrl: './shopping-list.component.css'
 })
-export class ShoppingListComponent {
+export class ShoppingListComponent implements OnInit{
   // custom property binding example
   // @Input() element: {type: string, name: string, content: string}
-  ingredients: Ingredient[] = [
-    new Ingredient('Apples', 5),
-    new Ingredient('Tomatos', 10),
-  ]
+  ingredients: Ingredient[]
+  
+  constructor(private slService: ShoppingListService){}
 
-  onIngredientAdded(ingredient: Ingredient) {
-    this.ingredients.push(ingredient)
+  ngOnInit(): void {
+      this.ingredients = this.slService.getIngredient()
+      this.slService.ingredientsChanged.subscribe((ingredients: Ingredient[]) => {
+        this.ingredients = ingredients
+      })
   }
 }

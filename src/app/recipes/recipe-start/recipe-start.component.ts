8f0d3791fb13ff302bad8dfc31615ab2c60a10c7
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-10 16:17:52 +0530

    adding child routing

---
+++ src/app/recipes/recipe-start/recipe-start.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-resipe-start',
+  templateUrl: './resipe-start.component.html',
+  styleUrl: './recipe-start.component.css'
+})
+export class RecipeStartComponent {
+
+}

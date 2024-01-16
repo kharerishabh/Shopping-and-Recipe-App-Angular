b2be274e77be068f41ea33b6191a50533a91353d
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-16 20:36:28 +0530

    syncing Html with the form

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -23,6 +23,9 @@ export class RecipeEditComponent implements OnInit{
       })
   }
 
+  onSubmit(){
+    console.log(this.recipeForm)
+  }
   private initForm() {
     let recipeName = '';
     let recipeImagePath = '';

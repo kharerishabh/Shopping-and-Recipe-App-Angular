d061e91749a6911041f411c687dfe0d05f80a512
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-28 23:35:51 +0530

    storing recipes in firebase

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,15 @@
 import { Component, EventEmitter, Output } from "@angular/core";
+import { DataStrorageService } from "../shared/data-storage.service";
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+    constructor(private dataStorageService: DataStrorageService){}
+
+    onSaveData(){
+        this.dataStorageService.storeRecipe()
+    }
+}
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -1,9 +1,18 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { RecipeService } from '../recipes/recipe.service';
 
-@Injectable({providedIn: 'root'})
-export class DataStrorageService{
-constructor(private http: HttpClient){
-    
-}
+@Injectable({ providedIn: 'root' })
+export class DataStrorageService {
+  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+
+  storeRecipe() {
+    const recipes = this.recipeService.getRecipe();
+    this.http
+      .put(
+        'https://angular-project-563c1-default-rtdb.firebaseio.com/recipes.json',
+        recipes
+      )
+      .subscribe((response) => console.log(response));
+  }
 }

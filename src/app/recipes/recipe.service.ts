e52dc8558fef95de62abb01ae9501f55d4a8a8ae
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-29 00:07:36 +0530

    trasforming response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -4,58 +4,57 @@ import { Ingredient } from '../shared/ingredient.model';
 import { ShoppingListService } from '../shopping-list/shopping-list.service';
 import { Subject } from 'rxjs';
 
-
 @Injectable()
 export class RecipeService {
-  recipeSubmitted = new Subject<Recipe[]>()
+  recipeSubmitted = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Choumein',
-      'A Super Tasty meal - just awesome',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnfv5hItZAm3iEpmEqhIUVrf_7dsAI829fBA&usqp=CAU',
-      [
-        new Ingredient('Meat', 1),
-        new Ingredient('French Fries', 20)
-      ]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'A Super Tasty and Cripsy',
-      'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnfv5hItZAm3iEpmEqhIUVrf_7dsAI829fBA&usqp=CAU',
-      [
-        new Ingredient('Buns', 2),
-        new Ingredient('Meat', 1)
-      ]
-    ),
-  ];
-
-  constructor(private slService: ShoppingListService){}
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Choumein',
+  //     'A Super Tasty meal - just awesome',
+  //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnfv5hItZAm3iEpmEqhIUVrf_7dsAI829fBA&usqp=CAU',
+  //     [
+  //       new Ingredient('Meat', 1),
+  //       new Ingredient('French Fries', 20)
+  //     ]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'A Super Tasty and Cripsy',
+  //     'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnfv5hItZAm3iEpmEqhIUVrf_7dsAI829fBA&usqp=CAU',
+  //     [
+  //       new Ingredient('Buns', 2),
+  //       new Ingredient('Meat', 1)
+  //     ]
+  //   ),
+  // ];
+  private recipes: Recipe[] = [];
+  constructor(private slService: ShoppingListService) {}
 
   setRecipes(recipe: Recipe[]) {
-    this.recipes = recipe
-    this.recipeSubmitted.next(this.recipes.slice())
+    this.recipes = recipe;
+    this.recipeSubmitted.next(this.recipes.slice());
   }
   getRecipe() {
     return this.recipes.slice();
   }
 
-  getRecipes(index: number){
-    return this.recipes[index]
+  getRecipes(index: number) {
+    return this.recipes[index];
   }
-  addIngredientsToShoppingList(ingredients: Ingredient[]){
-    this.slService.addIngredients(ingredients)
+  addIngredientsToShoppingList(ingredients: Ingredient[]) {
+    this.slService.addIngredients(ingredients);
   }
-  addRecipe(recipe:Recipe){
-    this.recipes.push(recipe)
-    this.recipeSubmitted.next(this.recipes.slice())
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipeSubmitted.next(this.recipes.slice());
   }
-  updateRecipe(index: number, newRecipe: Recipe){
-    this.recipes[index] = newRecipe
-    this.recipeSubmitted.next(this.recipes.slice())
+  updateRecipe(index: number, newRecipe: Recipe) {
+    this.recipes[index] = newRecipe;
+    this.recipeSubmitted.next(this.recipes.slice());
   }
-  deletRecipe(index: number){
+  deletRecipe(index: number) {
     this.recipes.splice(index, 1);
-    this.recipeSubmitted.next(this.recipes.slice())
+    this.recipeSubmitted.next(this.recipes.slice());
   }
 }
--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { map } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class DataStrorageService {
@@ -22,8 +23,18 @@ export class DataStrorageService {
       .get<Recipe[]>(
         'https://angular-project-563c1-default-rtdb.firebaseio.com/recipes.json'
       )
+      .pipe(
+        map((recipes) => {
+          return recipes.map((recipe) => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : [],
+            };
+          });
+        })
+      )
       .subscribe((recipes) => {
-        this.recipeService.setRecipes(recipes)
+        this.recipeService.setRecipes(recipes);
       });
   }
 }

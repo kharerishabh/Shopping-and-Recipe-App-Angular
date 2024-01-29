53ba617fb1a170505aa70dbf94594b4be3f512ea
KHARERISHABH <rishabkhare41@gmail.com>
2024-01-30 00:13:16 +0530

    emproving error handling using pipe and catchError method in service

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -33,9 +33,9 @@ export class AuthComponent {
           console.log(respData);
           this.isLoading = false
         },
-        error => {
-          console.log(error);
-          this.error = 'An Error Occured!'
+        errorMessage => {
+          console.log(errorMessage);
+          this.error = errorMessage
           this.isLoading = false
         }
       );
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
 
 interface AuthResponseData {
   idToken: string;
@@ -20,6 +21,16 @@ export class AuthService {
         password: password,
         returnSecuretoken: true,
       }
-    );
+    ).pipe(catchError(errorRes => {
+        let errorMessage = 'An unkown error occured';
+        if(!errorRes.error || !errorRes.error.error){
+            return throwError(errorMessage)
+        }
+        switch(errorRes.error.error.message){
+            case 'EMAIL_EXISTS':
+                errorMessage = 'This email already exists'
+        }
+        return throwError(errorMessage)
+    }))
   }
 }

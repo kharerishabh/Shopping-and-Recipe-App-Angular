04e946a32bdc513b4872501cbdae5da1e223319b
KHARERISHABH <rishabkhare41@gmail.com>
2024-02-04 17:12:57 +0530

    splitting the services from the app module and adding it to the coreModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { RecipesModule } from './recipes/recipes.module';
 import { RecipesRoutingModule } from './recipes/recipes-routing.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { SharedModule } from './shared/shared.module';
+import { CoreModule } from './core.module';
 
 @NgModule({
   declarations: [
@@ -30,16 +31,8 @@ import { SharedModule } from './shared/shared.module';
     RecipesModule,
     RecipesRoutingModule,
     ShoppingListModule,
-    SharedModule
-  ],
-  providers: [
-    ShoppingListService,
-    RecipeService,
-    {
-      provide: HTTP_INTERCEPTORS,
-      useClass: AuthInterceptorService,
-      multi: true,
-    },
+    SharedModule,
+    CoreModule
   ],
   bootstrap: [AppComponent],
 })

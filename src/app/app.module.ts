74d79751fb36b54053fb147a9fb2ece4bc4cd3b2
KHARERISHABH <rishabkhare41@gmail.com>
2024-02-14 12:21:02 +0530

    using the Store  devtools

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools'
 
 import { HeaderComponent } from './header/header.component';
 import { AppComponent } from './app.component';
@@ -11,6 +12,8 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { appReducer } from './store/app.reducer';
 import { AuthEffects } from './auth/store/auth.effects';
+import { environment } from '../environments/environment';
+
 
 @NgModule({
   declarations: [
@@ -25,6 +28,7 @@ import { AuthEffects } from './auth/store/auth.effects';
     CoreModule,
     StoreModule.forRoot(appReducer),
     EffectsModule.forRoot([AuthEffects]),
+    StoreDevtoolsModule.instrument({logOnly: environment.production})
   ],
   bootstrap: [AppComponent],
 })
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -8,14 +8,14 @@ import { environment } from '../../environments/environment';
 import { AppState } from '../store/app.reducer';
 import * as AuthActions from './store/auth.actions'
 
-export interface AuthResponseData {
-  idToken: string;
-  email: string;
-  refreshToken: string;
-  expiresIn: string;
-  localId: string;
-  registered?: string;
-}
+// export interface AuthResponseData {
+//   idToken: string;
+//   email: string;
+//   refreshToken: string;
+//   expiresIn: string;
+//   localId: string;
+//   registered?: string;
+// }
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   // user = new BehaviorSubject<User>(null);
